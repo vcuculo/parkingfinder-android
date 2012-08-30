b56ec2aa34e18c7249db92df45b35d8fd7a33e09
Antonio Luca <toolanto@gmail.com>
2012-08-30 22:39:29 +0200

    seekbar

--- src/mobidev/parkingfinder/OptionSettingsActivity.java
+++ src/mobidev/parkingfinder/OptionSettingsActivity.java
@@ -1,37 +1,56 @@
 package mobidev.parkingfinder;
 
 import android.app.Activity;
-import android.content.Intent;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.graphics.Canvas.EdgeType;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.SeekBar;
-import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.SeekBar.OnSeekBarChangeListener;
 import android.widget.ToggleButton;
 
 public class OptionSettingsActivity extends Activity{
-
-
-	private Spinner parkingTypeSpinner;
-	private ToggleButton audioToggleButton,filterToggleButton;
+	private final static String MY_PREFERENCES = "MyPref";
+	static final String PREFERENCE_RANGE="my_range";
+	static final String PREFERENCE_REFRESH="my_refresh";
+	static final String PREFERENCE_AUDIO="my_audio";
+	static final String PREFERENCE_FILTER="my_filter";//("0-1-2-3-4-5")//parse 
+	private final int MAX_RANGE_SEEK=800;
+	private final float MAX_REFRESH_SEEK=(float)4.5;
+	private final int MIN_RANGE_SEEK=200;
+	private final float MIN_REFRESH_SEEK=(float)0.5;
+	private ToggleButton audioToggleButton;
 	private SeekBar rangeSeekBar, timeSeekBar;
 	private Button saveButton,cancelButton;
-
+	private TextView valueRange,valueRefresh;
+	
+	private int currentRange;
+	float currentTime;
+	private int positionRangeCursor,positionTimeCursor;
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.option_settings);
+		SharedPreferences prefs = getSharedPreferences(MY_PREFERENCES,
+				Context.MODE_PRIVATE);
+		currentRange=prefs.getInt(PREFERENCE_RANGE, 300);
+		currentTime=prefs.getFloat(PREFERENCE_REFRESH, 3);
+		
 		audioToggleButton=(ToggleButton) findViewById(R.id.toggleNotify);
-		filterToggleButton=(ToggleButton) findViewById(R.id.toggleFilter);
 		rangeSeekBar=(SeekBar)findViewById(R.id.seekBarRange);
 		timeSeekBar=(SeekBar)findViewById(R.id.seekBarTime);
-		
-		parkingTypeSpinner = (Spinner) findViewById(R.id.spinnerFilter);
 		cancelButton = (Button) findViewById(R.id.buttonCancel);
 		saveButton = (Button) findViewById(R.id.buttonPositive);
-		parkingTypeSpinner.setSelection(1);
+		valueRange=(TextView) findViewById(R.id.textValueRange);
+		valueRange.setText(Integer.toString(currentRange));
+		valueRefresh=(TextView) findViewById(R.id.textValueTime);
+		valueRefresh.setText(Float.toString(currentTime));
 		cancelButton.setOnClickListener(new OnClickListener() {
 			@Override
 			public void onClick(View v) {
@@ -48,9 +67,60 @@ public class OptionSettingsActivity extends Activity{
 				finish();
 			}
 		});
-
-
-	
+		positionRangeCursor=currentRange-MIN_RANGE_SEEK;
+		rangeSeekBar.setMax(MAX_RANGE_SEEK);
+		rangeSeekBar.setProgress(positionRangeCursor);
+		rangeSeekBar.setOnSeekBarChangeListener(new OnSeekBarChangeListener() {
+			
+			@Override
+			public void onStopTrackingTouch(SeekBar seekBar) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void onStartTrackingTouch(SeekBar seekBar) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void onProgressChanged(SeekBar seekBar, int progress,
+					boolean fromUser) {
+				// TODO Auto-generated method stub
+				currentRange=progress+MIN_RANGE_SEEK;
+				valueRange.setText(Integer.toString(currentRange));
+				
+			}
+			
+			
+		
+		});
+		positionTimeCursor=(int)((currentTime-MIN_REFRESH_SEEK));
+		timeSeekBar.setMax((int)MAX_REFRESH_SEEK);
+		timeSeekBar.setProgress(positionTimeCursor);
+		timeSeekBar.setOnSeekBarChangeListener(new OnSeekBarChangeListener() {
+			
+			@Override
+			public void onStopTrackingTouch(SeekBar seekBar) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void onStartTrackingTouch(SeekBar seekBar) {
+				// TODO Auto-generated method stubMAX_REFRESH_SEEK/timeSeekBar.getMax())+
+				
+			}
+			
+			@Override
+			public void onProgressChanged(SeekBar seekBar, int progress,
+					boolean fromUser) {
+				// TODO Auto-generated method stub
+				currentTime=progress+MIN_REFRESH_SEEK;
+				valueRefresh.setText(Float.toString(currentTime));
+			}
+		});
 	}
 
 
