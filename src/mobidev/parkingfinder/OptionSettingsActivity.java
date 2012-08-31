272f1d29a4c7a3cb281b019fed55b7abec1ea4ba
Antonio Luca <toolanto@gmail.com>
2012-08-31 10:07:44 +0200

    activity optionSettings complete saved sharedpreference

--- src/mobidev/parkingfinder/OptionSettingsActivity.java
+++ src/mobidev/parkingfinder/OptionSettingsActivity.java
@@ -1,5 +1,7 @@
 package mobidev.parkingfinder;
 
+import java.util.HashMap;
+
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.Dialog;
@@ -21,7 +23,12 @@ public class OptionSettingsActivity extends Activity implements OnClickListener{
 	static final String PREFERENCE_RANGE="my_range";
 	static final String PREFERENCE_REFRESH="my_refresh";
 	static final String PREFERENCE_AUDIO="my_audio";
-	static final String PREFERENCE_FILTER="my_filter";//("0-1-2-3-4-5")//parse 
+	static final String PREFERENCE_FILTER_UNDEFINED="undefined";
+	static final String PREFERENCE_FILTER_FREE="free";
+	static final String PREFERENCE_FILTER_TOLL="toll";
+	static final String PREFERENCE_FILTER_RESIDENT="resident";
+	static final String PREFERENCE_FILTER_DISABLED="disabled";
+	static final String PREFERENCE_FILTER_TIMED="timed";//("0-1-2-3-4-5")//parse 
 	private final int MAX_RANGE_SEEK=800;
 	private final float MAX_REFRESH_SEEK=(float)4.5;
 	private final int MIN_RANGE_SEEK=200;
@@ -32,12 +39,13 @@ public class OptionSettingsActivity extends Activity implements OnClickListener{
 	private ToggleButton audioToggleButton;
 	private SeekBar rangeSeekBar, timeSeekBar;
 	private Button saveButton,cancelButton,filterButton;
-	private TextView valueRange,valueRefresh;
+	private TextView valueRange,valueRefresh,valueFilter;
 	
 	private int currentRange;
 	private float currentTime;
 	private boolean currentAudio;
-	private String currentFilter;
+	private boolean currentFUndefined,currentFFree,currentFToll,currentFResident;
+	private boolean currentFDisabled,currentFTimed;
 	
 	private int positionRangeCursor,positionTimeCursor;
 	@Override
@@ -49,6 +57,12 @@ public class OptionSettingsActivity extends Activity implements OnClickListener{
 		currentRange=prefs.getInt(PREFERENCE_RANGE, 300);
 		currentTime=prefs.getFloat(PREFERENCE_REFRESH, 3);
 		currentAudio=prefs.getBoolean(PREFERENCE_AUDIO, true);
+		currentFUndefined=prefs.getBoolean(PREFERENCE_FILTER_UNDEFINED, false);
+		currentFFree=prefs.getBoolean(PREFERENCE_FILTER_FREE, false);
+		currentFToll=prefs.getBoolean(PREFERENCE_FILTER_TOLL, false);
+		currentFResident=prefs.getBoolean(PREFERENCE_FILTER_RESIDENT, false);
+		currentFDisabled=prefs.getBoolean(PREFERENCE_FILTER_DISABLED, false);
+		currentFTimed=prefs.getBoolean(PREFERENCE_FILTER_TIMED, false);
 		
 		audioToggleButton=(ToggleButton) findViewById(R.id.toggleNotify);
 		rangeSeekBar=(SeekBar)findViewById(R.id.seekBarRange);
@@ -64,8 +78,7 @@ public class OptionSettingsActivity extends Activity implements OnClickListener{
 		saveButton.setOnClickListener(this);
 		filterButton.setOnClickListener(this);
 		audioToggleButton.setChecked(currentAudio);
-		
-		
+
 		//seekbar
 		positionRangeCursor=currentRange-MIN_RANGE_SEEK;
 		rangeSeekBar.setMax(MAX_RANGE_SEEK);
@@ -127,18 +140,64 @@ public class OptionSettingsActivity extends Activity implements OnClickListener{
 	public Dialog onCreateDialog(int id){
 		switch (id) {
 		case FILTER_DIALOG:
-			final CharSequence[] items = {"0", "1", "2","3","4","5"};
-
+			final CharSequence[] items =getResources().getStringArray(R.array.parkingTypes);
+			boolean undefined=(currentFUndefined?true:false);
+			boolean free=(currentFFree?true:false);
+			boolean toll=(currentFToll?true:false);
+			boolean residents=(currentFResident?true:false);
+			boolean disabled=(currentFDisabled?true:false);
+			boolean timed=(currentFTimed?true:false);
+			boolean[] areChecked={undefined,free,toll,residents,disabled,timed};
 			AlertDialog.Builder builder = new AlertDialog.Builder(this);
 			builder.setTitle("Check type");
-			builder.setMultiChoiceItems(items, null, new DialogInterface.OnMultiChoiceClickListener() {
+			builder.setMultiChoiceItems(items, areChecked, new DialogInterface.OnMultiChoiceClickListener() {
 				
 				@Override
 				public void onClick(DialogInterface dialog, int which, boolean isChecked) {
 					// TODO Auto-generated method stub
-					
+					switch (which) {
+					case 0:
+						if(isChecked)
+						currentFUndefined=true;
+						else
+						currentFUndefined=false;
+						break;
+					case 1:
+						if(isChecked)
+						currentFFree=true;
+						else
+						currentFFree=false;
+						break;
+					case 2:
+						if(isChecked)
+						currentFToll=true;
+						else
+						currentFToll=false;
+						break;
+					case 3:
+						if(isChecked)
+						currentFResident=true;
+						else
+						currentFResident=false;
+						break;	
+					case 4:
+						if(isChecked)
+						currentFDisabled=true;
+						else
+						currentFDisabled=false;
+						break;
+					case 5:
+						if(isChecked)
+						currentFTimed=true;
+						else
+						currentFTimed=false;
+						break;
+					default:
+						break;
+					}
 				}
 			});
+			
 			AlertDialog alert = builder.create();
 			return alert;
 			
@@ -153,13 +212,14 @@ public class OptionSettingsActivity extends Activity implements OnClickListener{
 		// TODO Auto-generated method stub
 		int id=v.getId();
 		switch (id) {
-		case R.id.saveButton:
+		case R.id.buttonPositive:
+			savePreferences();
 			setResult(RESULT_OK);
-			finish();
+			this.finish();
 			break;
-		case R.id.cancelButton:
+		case R.id.buttonCancel:
 			setResult(RESULT_OK);
-			finish();
+			this.finish();
 			break;
 		case R.id.buttonFilter:
 			showDialog(FILTER_DIALOG);
@@ -168,6 +228,25 @@ public class OptionSettingsActivity extends Activity implements OnClickListener{
 			break;
 		}	
 	}
+
+	private void savePreferences(){
+		
+		SharedPreferences prefs = getSharedPreferences(MY_PREFERENCES,
+				Context.MODE_PRIVATE);
+		SharedPreferences.Editor editor = prefs.edit();
+		editor.putInt(PREFERENCE_RANGE, currentRange);
+		editor.putFloat(PREFERENCE_REFRESH, currentTime);
+		currentAudio=audioToggleButton.isChecked();
+		editor.putBoolean(PREFERENCE_AUDIO, currentAudio);
+		editor.putBoolean(PREFERENCE_FILTER_UNDEFINED, currentFUndefined);
+		editor.putBoolean(PREFERENCE_FILTER_FREE, currentFFree);
+		editor.putBoolean(PREFERENCE_FILTER_TOLL, currentFToll);
+		editor.putBoolean(PREFERENCE_FILTER_RESIDENT, currentFResident);
+		editor.putBoolean(PREFERENCE_FILTER_DISABLED, currentFDisabled);
+		editor.putBoolean(PREFERENCE_FILTER_TIMED, currentFTimed);
+		editor.commit();
+	}
+	
 
 
 }
